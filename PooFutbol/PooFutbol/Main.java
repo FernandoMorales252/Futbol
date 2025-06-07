0a67b5476d1c0b0d8c136ec8d786f7ebee8d2c26
Fernando Morales <Fernandomoe09@outlook.com>
2025-06-06 20:50:27 -0600

    cambios

--- PooFutbol/PooFutbol/Main.java
+++ PooFutbol/PooFutbol/Main.java
@@ -3,8 +3,8 @@ package PooFutbol;
 public class Main {
     public static void main(String[] args) {
         // Crear instancias de cada clase
-        Futbolista futbolista = new Futbolista(1, "Lionel", "Messi", 34, 10, "Delantero");
-        Entrenador entrenador = new Entrenador(2, "Pep", "Guardiola", 50, "12345");
+        Futbolista futbolista = new Futbolista(1, "Lionel", "Messi", 34, 10, "Extremo");
+        Entrenador entrenador = new Entrenador(2, "Luis", "Enrique", 50, "12345");
         Masajista masajista = new Masajista(3, "Juan", "Pérez", 40, "Fisioterapia", 15);
 
         // Llamar a los métodos de cada instancia
